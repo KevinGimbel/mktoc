a54cfc37863bdd8a4322770589d78cf9bd479b9e
Kevin Gimbel <hallo@kevingimbel.com>
2019-12-04 17:53:38 +0100

    Fix #1, add depth arguments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # `mktoc`
-> Blazingly fast Table of Content generator
+> Blazingly fast Markdown Table of Content generator
 
 ![](https://github.com/kevingimbel/mktoc/workflows/Clippy%20check/badge.svg)
 
@@ -16,11 +16,11 @@
 
 ## About
 
-`mktoc` parses markdown files and generates a Table Of Content linking all headlines up to heading level 6 deep.
+`mktoc` parses markdown files and generates a Table Of Content linking all headlines up to heading level 6 deep, or as specified by command line arguments. A start depth and maximum depth can be specified.
 
 ## Installation
 
-`mktoc` can be installed using Cargo, the Rust package manager, or by downloading a binary from GitHub.
+`mktoc` can be installed using Cargo, the Rust package manager.
 
 ### Cargo
 
@@ -30,20 +30,43 @@ $ cargo install mktoc
 
 ### Binary
 
-Download latest release from [https://github.com/kevingimbel/mktoc/releases](https://github.com/kevingimbel/mktoc/releases) and place it somewhere in your `PATH`, e.g. `/usr/local/bin`.
+Binaries are actually not available yet. If you know how releasing binaries with Rust can be implemented, please let me know!
 
 ## Usage
 
 Specify `--write` to overwrite the given file, otherwise the modified content is written to stdout.
 
 ```
-# mktoc [--write] <FILE>
+# mktoc [--write] [--max-depth|-M] [--min-depth|-m] <FILE>
 $ mktoc --write README.md
+$ mktoc --write -m 2 -M 4 README.md
 ```
 
 See `mktoc --help` for list of all arguments and flags.
 
+```
+mktoc 1.1.0
+
+USAGE:
+    mktoc [FLAGS] [OPTIONS] <file>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+    -w, --write      
+
+OPTIONS:
+    -M, --max-depth <max-depth>     [default: 6]
+    -m, --min-depth <min-depth>     [default: 1]
+
+ARGS:
+    <file> 
+```
  
 ## Performance
 
-`mktoc` is blazingly fast. Large files such as the README examples in `tests/files/` render in 0.009s (9ms) on average.
+`mktoc` is fast but can probably be even faster! Pull Requests and bug reports are appreciated!
+
+## License
+
+MIT, see LICENSE file.
--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ struct Cli {
 const COMMENT_BEGIN: &str = "<!-- BEGIN mktoc -->";
 const COMMENT_END: &str = "<!-- END mktoc -->";
 
+/// reads a file into a mutable string
 fn read_file(file_path: String) -> Result<String, ::std::io::Error> {
     let mut file = File::open(file_path)?;
     let mut contents = String::new();
@@ -23,33 +24,62 @@ fn read_file(file_path: String) -> Result<String, ::std::io::Error> {
 
     Ok(contents)
 }
-
-fn generate_toc(original_content: String) -> String {
-    // @TODO: This RegEx creates weird outputs if the headline contains additional markdown, like images
-    let re = regex::Regex::new(r"((#{1,6}\s))((.*))").unwrap();
+/// parses a string and extracts all headlines to build a table of contents
+///
+/// Uses basic regex "((#{1,6}\s))((.*))" to parse headings. Right now this produces errors because it also matches
+/// comments in code blocks and if the headline contains images those will be matched, too.
+pub fn generate_toc(original_content: String, min_depth: i32, max_depth: i32) -> String {
+    let mut already_found_code_open = false;
+    let mut code_block_found = false;
     let mut new_toc = String::from(COMMENT_BEGIN);
-    // let caps = re.captures(content.as_str());
-    for caps in re.captures_iter(original_content.as_str()) {
-        let level: usize = caps.get(2).unwrap().as_str().chars().count() - 1;
-        let text = caps.get(3).unwrap().as_str();
-        // @TODO: Use real URL encoding
-        let link = text.replace(" ", "-").to_ascii_lowercase();
-        // let spaces = " ".repeat(level -1);
-        let spaces = match level {
-            3 => String::from("  "),
-            4 => String::from("    "),
-            5 => String::from("      "),
-            6 => String::from("        "),
-            _ => String::from(""),
-        };
+    let re = regex::Regex::new(r"((#{1,6}\s))((.*))").unwrap();
+    for line in original_content.lines() {
 
-        new_toc = format!(
-            "{old}\n{spaces}- [{text}](#{link})",
-            old = new_toc,
-            spaces = spaces,
-            text = text,
-            link = link
-        );
+        if line.starts_with("```") {
+            code_block_found = true;
+        }
+
+        if !code_block_found && !already_found_code_open {
+            if line.starts_with("#") {
+                let caps = re.captures(line).unwrap();
+                let level: i32 = (caps.get(2).unwrap().as_str().chars().count() - 1) as i32;
+                if level < min_depth {
+                    continue;
+                }
+
+                if level > max_depth {
+                    continue;
+                }
+
+
+                let text = caps.get(3).unwrap().as_str();
+                let link = text.replace(" ", "-").to_ascii_lowercase();
+                let spaces = match level {
+                    3 => String::from("  "),
+                    4 => String::from("    "),
+                    5 => String::from("      "),
+                    6 => String::from("        "),
+                    _ => String::from(""),
+                };
+                new_toc = format!(
+                    "{old}\n{spaces}- [{text}](#{link})",
+                    old = new_toc.as_str(),
+                    spaces = spaces,
+                    text = text,
+                    link = link
+                );
+            }
+        }
+
+        if code_block_found && already_found_code_open {
+            code_block_found = false;
+            already_found_code_open = false;
+        }
+
+        if line.starts_with("```") {
+            already_found_code_open = true;
+        }
+
     }
 
     new_toc = format!("{}\n{}", new_toc, COMMENT_END);
@@ -57,9 +87,10 @@ fn generate_toc(original_content: String) -> String {
     new_toc
 }
 
-pub fn make_toc(file_path_in: String) -> Result<String, ::std::io::Error> {
+/// takes a file path as `String` and returns a table of contents for the file
+pub fn make_toc(file_path_in: String, min_depth: i32, max_depth: i32) -> Result<String, ::std::io::Error> {
     let content = read_file(file_path_in)?;
-    let new_toc = generate_toc(content.to_owned());
+    let new_toc = generate_toc(content.to_owned(), min_depth, max_depth);
     let re_toc = regex::Regex::new(r"(?ms)^(<!-- BEGIN mktoc).*(END mktoc -->)").unwrap();
     let res: String = re_toc
         .replace_all(content.as_str(), new_toc.as_str())
