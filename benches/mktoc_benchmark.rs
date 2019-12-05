58a8f27890f5694d0a8a2122016f27ee67c26756
Kevin Gimbel <hallo@kevingimbel.com>
2019-12-05 10:46:56 +0100

    Update benchs and docs

--- benches/mktoc_benchmark.rs
+++ benches/mktoc_benchmark.rs
@@ -9,13 +9,13 @@ use mktoc::*;
 
 fn mktoc_benchmark(c: &mut Criterion) {
     c.bench_function("mktoc: tests/files/README_01.md", |b| {
-        b.iter(|| make_toc(String::from("../tests/files/README_01.md")))
+        b.iter(|| make_toc(String::from("../tests/files/README_01.md"), 1, 6))
     });
     c.bench_function("mktoc: tests/files/README_02.md", |b| {
-        b.iter(|| make_toc(String::from("../tests/files/README_02.md")))
+        b.iter(|| make_toc(String::from("../tests/files/README_02.md"), 1,6))
     });
     c.bench_function("mktoc: tests/files/README_03.md", |b| {
-        b.iter(|| make_toc(String::from("../tests/files/README_03.md")))
+        b.iter(|| make_toc(String::from("../tests/files/README_03.md"),1 ,6))
     });
 }
 
