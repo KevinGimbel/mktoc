8067ba38d871c01c53bbad4782227392cbed6484
Kevin Gimbel <kevin@gimbel.dev>
2023-04-13 09:20:26 +0200

    test: add tests for text_to_url

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,22 @@ struct Config {
     max_depth: i32,
 }
 
+impl Config {
+    // ensure_min_max sets the max_depth and min_depth values to 
+    // their defaults in case they were configured wrong.
+    fn ensure_min_max(&mut self) {
+        if (self.max_depth > 6) || (self.max_depth < 1)  {
+             self.max_depth = default_max_depth();
+             eprintln!("WARNING: max_depthout of boound. Default value '{}' used.", self.max_depth);
+        }
+        
+        if (self.min_depth < 1) || (self.min_depth > 6)  {
+            self.min_depth = default_min_depth();
+            eprintln!("WARNING: min_depthout of boound. Default value '{}' used.", self.min_depth);
+        }
+    }
+}
+
 impl Default for Config {
     fn default() -> Self {
         Self { min_depth: default_min_depth(), max_depth: default_max_depth() }
@@ -158,10 +174,13 @@ fn parse_json_config_and_begin_comment<'t>(text: &'t str) -> (Config, String) {
     }
 
     // parse the JSON string as a Config struct
-    let config: Config = match serde_json::from_str(json_str) {
+    let mut config: Config = match serde_json::from_str(json_str) {
         Ok(config) => config,
         Err(_e) => { Config::default() }
     };
+
+    // ensures the min_depth and max_depth are within scope
+    config.ensure_min_max();
 
     return (config, start_comment)
 }
@@ -207,21 +226,101 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_text_to_url() {
+        struct TestCase<'a> {
+            name: &'a str,
+            input: &'a str,
+            expected: String,
+        }
+
+        let tests = vec![
+            TestCase{
+                name: "Case 01: My heading",
+                input: "My heading",
+                expected: String::from("my-heading")
+            },TestCase{
+                name: "Case 02: My Heading with `code`",
+                input: "My Heading with `code`",
+                expected: String::from("my-heading-with-code")
+            },TestCase{
+                name: "Case 03: Emoji 💃",
+                input: "Emoji 💃",
+                expected: String::from("emoji-💃")
+            },TestCase{
+                name: "Case 03: Number sign",
+                input: "Why #5 is nice",
+                expected: String::from("why-5-is-nice")
+            },TestCase{
+                name: "Case 04: Special signs",
+                input: "Reasons to say !@;#\\!! out loud",
+                expected: String::from("reasons-to-say-@-out-loud")
+            },TestCase{
+                name: "Case 05: Empty",
+                input: "",
+                expected: String::from("")
+            }
+        ];
+
+        for test in tests {
+            dbg!(test.name);
+            assert_eq!(text_to_url(test.input), test.expected);
+        }
+    }
+
+    #[test]
     fn test_parse_json_config_and_begin_comment() {
         struct TestCase<'a> {
+            name: &'a str,
             input: &'a str,
             expected: Config,
         }
 
-        let tests = vec![TestCase{
+        let tests = vec![
+        TestCase{
+            name: "only min_depth set",
             input: "<!-- BEGIN mktoc {\"min_depth\":3} -->",
             expected: Config{
                 min_depth: 3,
                 max_depth: 6
             }
-        }];
+        },
+        TestCase{
+            name: "min_depth and max_depth set",
+            input: "<!-- BEGIN mktoc {\"min_depth\":3, \"max_depth\":4} -->",
+            expected: Config{
+                min_depth: 3,
+                max_depth: 4
+            }
+        },
+        TestCase{
+            name: "only max_depth set",
+            input: "<!-- BEGIN mktoc {\"max_depth\":4} -->",
+            expected: Config{
+                min_depth: 1,
+                max_depth: 4
+            }
+        },
+        TestCase{
+            name: "no json config, returns default values",
+            input: "<!-- BEGIN mktoc -->",
+            expected: Config{
+                min_depth: 1,
+                max_depth: 6
+            }
+        },
+        TestCase{
+            name: "invalid max_depth set results in default max_depth being used",
+            input: "<!-- BEGIN mktoc {\"max_depth\":10} -->",
+            expected: Config{
+                min_depth: 1,
+                max_depth: 6
+            }
+        },
+        ];
 
         for test in tests {
+            // logs the name of the test in case it fails.
+            dbg!(test.name);
             let (cnf, comment) = parse_json_config_and_begin_comment(&test.input);
             assert_eq!(cnf.max_depth, test.expected.max_depth);
             assert_eq!(cnf.min_depth, test.expected.min_depth);
