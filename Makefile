b6cf6218784e4c81fd56dac9fb9e05853a3082a9
Kevin Gimbel <kevin@gimbel.dev>
2023-04-14 08:14:39 +0200

    chore: update criterion to 0.4

--- Makefile
+++ Makefile
@@ -2,12 +2,9 @@
 
 bench:
 	@cargo bench
+
 build:
 	@cargo build --release
 
 docs:
 	@cargo doc
-
-frontend:
-	@cp -r ./target/criterion docs/benches/
-	@cp -r ./target/doc docs/
--- benches/mktoc_benchmark.rs
+++ benches/mktoc_benchmark.rs
@@ -6,17 +6,15 @@ use criterion::Criterion;
 extern crate mktoc;
 
 use mktoc::*;
+use std::fs;
 
 fn mktoc_benchmark(c: &mut Criterion) {
-    c.bench_function("mktoc: tests/files/README_01.md", |b| {
-        b.iter(|| make_toc(String::from("../tests/files/README_01.md"), 1, 6))
-    });
-    c.bench_function("mktoc: tests/files/README_02.md", |b| {
-        b.iter(|| make_toc(String::from("../tests/files/README_02.md"), 1, 6))
-    });
-    c.bench_function("mktoc: tests/files/README_03.md", |b| {
-        b.iter(|| make_toc(String::from("../tests/files/README_03.md"), 1, 6))
-    });
+    for entry in fs::read_dir("tests/files/").unwrap() {
+        let file = entry.unwrap();
+        c.bench_function(format!("mktoc:{:?}", file.path()).as_str(), |b| {
+            b.iter(|| make_toc(file.path(), 1, 6))
+        });    
+    }
 }
 
 criterion_group!(benches, mktoc_benchmark);
